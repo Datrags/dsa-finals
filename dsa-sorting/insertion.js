c112bd340aafede9dc9cfb4b18cff227f0d95524
Stephan Green <stephangreen@Stephans-MacBook-Air.local>
2024-11-03 10:36:54 -0500

    mergesort

--- dsa-sorting/insertion.js
+++ dsa-sorting/insertion.js
@@ -1,3 +1,16 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        // Shift elements 
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        // Place the current element in its correct position
+        arr[j + 1] = current;
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
--- dsa-sorting/merge.js
+++ dsa-sorting/merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(arr1, arr2) {
 
-function mergeSort() {}
+    let newArr = [];
+    let i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            newArr.push(arr1[i]);
+            i++;
+        } else {
+            newArr.push(arr2[j]);
+            j++;
+        }
+    }
+    if (i < arr1.length) {
+        newArr.push(...arr1.slice(i));
+    }
+    if (j < arr2.length) {
+        newArr.push(...arr2.slice(j));
+    }
+
+    return newArr;
+}
+
+function mergeSort(arr) {
+
+    if (arr.length <= 1) return arr;
+
+    let arr1 = arr.slice(0, Math.floor(arr.length / 2));
+    let arr2 = arr.slice(arr.length / 2);
+
+    let sortLeft = mergeSort(arr1);
+    let sortRight = mergeSort(arr2);
+
+    return merge(sortLeft, sortRight);
+}
 
 module.exports = { merge, mergeSort};
